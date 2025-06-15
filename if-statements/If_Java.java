f30f90984fddd73cfe59acb571b3b2677bb6daef
MbalieP <mbphulwjhb024@student.wethinkcode.co.za>
2025-06-15 19:53:40 +0200

    Create If_Java.java

---
+++ if-statements/If_Java.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class IfJava {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter your age: ");
+        int age = input.nextInt();
+
+        if (age < 18) {
+            System.out.println("You are a minor.");
+        } else if (age >= 18 && age < 60) {
+            System.out.println("You are an adult.");
+        } else {
+            System.out.println("You are a senior.");
+        }
+    }
+}
