6c2f9e354f2d53f7ec4337f2908b78ce4adb2098
MbalieP <mbphulwjhb024@student.wethinkcode.co.za>
2025-06-16 11:04:47 +0200

    Update and rename Java_arrays to Java_arrays.java

---
+++ Arrays/Java_arrays.java
@@ -0,0 +1,19 @@
+
+public class Arrays {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5};
+        System.out.println("Java 1D Array:");
+        for (int num : numbers) {
+            System.out.println(num);
+        }
+    }
+}
+public class MultiDimensionalArrays {
+    public static void main(String[] args) {
+        int[][] matrix = {
+            {1, 2, 3},
+            {4, 5, 6}
+        };
+        System.out.println("Java 2D Array Element [1][2]: " + matrix[1][2]);
+    }
+}
