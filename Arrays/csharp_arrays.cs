33b939b15109a0deea5901e15193f9342bdbddf7
MbalieP <mbphulwjhb024@student.wethinkcode.co.za>
2025-06-16 11:05:11 +0200

    Update and rename csharp_arrays to csharp_arrays.cs

---
+++ Arrays/csharp_arrays.cs
@@ -0,0 +1,12 @@
+
+using System;
+
+class Arrays {
+    static void Main() {
+        int[] numbers = {1, 2, 3, 4, 5};
+        Console.WriteLine("C# 1D Array:");
+        foreach (int num in numbers) {
+            Console.WriteLine(num);
+        }
+    }
+}
