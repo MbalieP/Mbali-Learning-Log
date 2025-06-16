01c05930b12c3f37bb8c4d06079a2a67adfebe40
MbalieP <mbphulwjhb024@student.wethinkcode.co.za>
2025-06-16 11:05:44 +0200

    Update csharp_arrays.cs

--- Arrays/csharp_arrays.cs
+++ Arrays/csharp_arrays.cs
@@ -10,3 +10,15 @@ class Arrays {
         }
     }
 }
+using System;
+
+class MultiDimensionalArrays {
+    static void Main() {
+        int[,] matrix = {
+            {1, 2, 3},
+            {4, 5, 6}
+        };
+        Console.WriteLine("C# 2D Array Element [1,2]: " + matrix[1, 2]);
+    }
+}
+
