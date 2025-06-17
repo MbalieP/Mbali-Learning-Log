0789d0f2846c7564d0b1c8c94f1fabc5e2a4ae60
MbalieP <mbphulwjhb024@student.wethinkcode.co.za>
2025-06-17 08:55:30 +0200

    Update JavaMethods.java

--- Methods/JavaMethods.java
+++ Methods/JavaMethods.java
@@ -1,1 +1,52 @@
+class Animal {
+    public void speak() {
+        System.out.println("The animal makes a sound.");
+    }
+}
+
+class Dog extends Animal {
+    @Override
+    public void speak() {
+        System.out.println("The dog barks.");
+    }
+}
+
+class Calculator {
+    // Method Overloading
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public double add(double a, double b) {
+        return a + b;
+    }
+}
+
+class Person {
+    // Getter and Setter
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
+
+public class JavaMethods {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        dog.speak();
+
+        Calculator calc = new Calculator();
+        System.out.println(calc.add(5, 10));
+        System.out.println(calc.add(5.5, 4.5));
+
+        Person p = new Person();
+        p.setName("Mbali");
+        System.out.println(p.getName());
+    }
+}
 
