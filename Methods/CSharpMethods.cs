014c16fa49c8ba0cab1deb40bff41274a7e12a1a
MbalieP <mbphulwjhb024@student.wethinkcode.co.za>
2025-06-17 08:54:45 +0200

    Update CSharpMethods.cs

--- Methods/CSharpMethods.cs
+++ Methods/CSharpMethods.cs
@@ -1,1 +1,48 @@
+using System;
+
+class Animal
+{
+    public virtual void Speak()
+    {
+        Console.WriteLine("The animal makes a sound.");
+    }
+}
+
+class Dog : Animal
+{
+    public override void Speak()
+    {
+        Console.WriteLine("The dog barks.");
+    }
+}
+
+class Calculator
+{
+    // Method Overloading
+    public int Add(int a, int b) => a + b;
+    public double Add(double a, double b) => a + b;
+}
+
+class Person
+{
+    // Getter and Setter using properties
+    public string Name { get; set; }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Dog dog = new Dog();
+        dog.Speak();
+
+        Calculator calc = new Calculator();
+        Console.WriteLine(calc.Add(5, 10));
+        Console.WriteLine(calc.Add(5.5, 4.5));
+
+        Person p = new Person();
+        p.Name = "Mbali";
+        Console.WriteLine(p.Name);
+    }
+}
 
